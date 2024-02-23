0b38a35a2aef4e2aaab7e337309445f9891a6587
Jean Heberth <jeanheberth19@gmail.com>
2024-02-23 11:12:25 -0300

    Initial commit

---
+++ src/main/java/br/com/drogaria/drogariaspring/DrogariaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.drogaria.drogariaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogariaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrogariaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/drogaria/drogariaspring/DrogariaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drogaria.drogariaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogariaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
